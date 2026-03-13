8ac56deeb8d84dbf55ec7cd3e966702ad7eac9f7
fisaxili <anfisavoro@gmail.com>
2026-03-14 01:08:47 +0300

    Создание формы. Добавление основных элементов: Form (форма), Button (кнопка), TextBox (поле ввода), Label (метка), ListBox (список), PictureBox (область для графики).

--- Lab02_Variant03/Form1.cs
+++ Lab02_Variant03/Form1.cs
@@ -16,5 +16,10 @@ namespace Lab02_Variant03
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
